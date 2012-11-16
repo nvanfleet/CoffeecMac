a4751cef92030f8b2ea73b3610e5d60ee4567bf4
Nathan Van Fleet <nmcdavit@octopus.lan>
2012-11-15 22:25:36 -0500

    Initial Commit

---
+++ coffeec/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  coffeec
+//
+//  Created by Nathan Van Fleet on 2012-11-15.
+//  Copyright (c) 2012 Nathan Van Fleet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
